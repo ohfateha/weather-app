2faf5b6e6f598554694cc4d61e7fb1975ff209c7
ohfateha <nurfateha03@gmail.com>
2023-11-15 17:11:21 +0800

    forecast weather

--- src/index.js
+++ src/index.js
@@ -15,18 +15,21 @@ function displayTemperature(response) {
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" alt="weather-icon">`;
   timeElement.innerHTML = formatDate(date);
+
+  getForcast(response.data.city);
 }
 
-
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
-
+function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  searchCity = searchInputElement.value;
 }
 
 function formatDate(date) {
@@ -56,6 +59,46 @@ function formatDate(date) {
   return `${formattedDay} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForcast(city) {
+  let apiKey = "ade4b83da47e447770f3029co72835bt";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForcast);
+}
+
+function displayForcast(response) {
+  let forcastHTML = "";
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forcastHTML =
+        forcastHTML +
+        `<div class="forcast-day">
+        <div class="forcast-date">${formatDay(day.time)}</div>
+
+        <img src="${day.condition.icon_url}" class="forcast-icon" />
+        <div class="forcast-temp">
+          <div class="forcast-temp-max">
+            ${Math.round(day.temperature.maximum)}º
+          </div>
+          <div class="forcast-temp-min">${Math.round(
+            day.temperature.minimum
+          )}º</div>
+        </div>
+      </div>`;
+    }
+  });
+
+  let forcastElement = document.querySelector("#forcast");
+  forcastElement.innerHTML = forcastHTML;
+}
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
@@ -63,3 +106,4 @@ let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+searchCity("Johor Bahru");
